211fb73489cf1fb08e457a4587fca05e5358c92e
Jaskowski, Sebastian Patrick <jaskowskiseb@gmail.com>
2024-03-28 18:26:03 -0400

    implemented mamba model, boilerplate for other ralms
    implemented mamba model, boilerplate for other ralms

---
+++ src/dolly_ralm.py
@@ -0,0 +1,17 @@
+from ralm import RALM
+
+class DollyRALM(RALM):
+    '''RALM class dedicated to wrapping a Transformer architecture (Dolly) for RAG tasks'''
+    def __init__(self, pretrained_prefix : str):
+        super(RALM, self).__init__()
+        # TODO: Instantiate mamba model
+        self.lm = None
+
+    def format_output(self, output : str) -> str:
+        pass
+        # TODO: write this
+
+    def predict(self, question : str) -> str:
+        prompt = self.generate_prompt(question)
+        response = self.lm(prompt) # TODO: This may be called differently
+        return self.format_output(response)
