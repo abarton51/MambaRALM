77fb4c957e85e9a7f66c0278c6b255d870b0526e
Jaskowski, Sebastian Patrick <jaskowskiseb@gmail.com>
2024-03-29 17:19:04 -0400

    fixed some prompting stuff
    fixed some prompting stuff

--- src/mamba_ralm.py
+++ src/mamba_ralm.py
@@ -1,20 +1,23 @@
 from ralm import RALM
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from mamba_ssm.models.mixer_seq_simple import MambaLMHeadModel
+from langchain_core.vectorstores import VectorStore
 from src.config import device
 from typing import Any
 
 class MambaRALM(RALM):
     '''RALM class dedicated to wrapping a Mamba architecture for RAG tasks'''
-    def __init__(self, pretrained_prefix : str, vector_db : Any):
+    def __init__(self, pretrained_prefix : str = "havenhq/mamba-chat", vector_db : VectorStore = None):
         super().__init__(vector_db)
         # self.lm = MambaLMHeadModel.from_pretrained(pretrained_prefix, device=device)
         self.lm = AutoModelForCausalLM.from_pretrained(pretrained_prefix, device=device)
         self.tokenizer = AutoTokenizer.from_pretrained(pretrained_prefix)
 
+    @override
     def format_output(self, output : list[str]) -> str:
         return output[0].split("<|assistant|>\n")[1].split("<|endoftext|>\n")[0]
 
+    @override
     def predict(self, question : str, max_response_length : int = 150) -> str:
         prompt = self.generate_prompt(question)
         input_ids = self.tokenizer(prompt, return_tensors="pt")['input_ids'].to(device)
