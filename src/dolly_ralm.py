5fff040d2950e9356a8ba6fe7073de86c7773e52
NMBridges <nolanmbridges@gmail.com>
2024-03-31 19:31:06 -0400

    updated main

--- src/dolly_ralm.py
+++ src/dolly_ralm.py
@@ -10,7 +10,7 @@ class DollyRALM(RALM):
     def __init__(self, pretrained_prefix : str = "databricks/dolly-v2-3b", vector_db : VectorStore = None):
         super().__init__(vector_db)
         # TODO: Instantiate Dolly RALM model
-        self.lm = pipeline(model="databricks/dolly-v2-3b", torch_dtype=torch.bfloat16, trust_remote_code=True, device_map=device)
+        self.lm = pipeline(model=pretrained_prefix, torch_dtype=torch.bfloat16, trust_remote_code=True, device_map=device)
 
     def format_output(self, output : str) -> str:
         return output[0]["generated_text"]
