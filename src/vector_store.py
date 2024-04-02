e09ba4fc027a88693db4c0fd4ee7efde8b58b2e4
Austin Barton <101911867+abarton51@users.noreply.github.com>
2024-04-01 21:23:46 -0400

    Update vector_store.py

--- src/vector_store.py
+++ src/vector_store.py
@@ -101,7 +101,7 @@ class RAGVectorStore:
         
         return list(itertools.chain.from_iterable(self.loader.load()))
 
-    def chunk_to_json(self, docs: Iterable[Document], file_path: str = None) -> None:
+    def chunk_to_json(self, docs: Iterable[Document], json_path: str = None) -> None:
         #suppress warnings for text splitting
         warnings.filterwarnings("ignore")
 
@@ -109,7 +109,7 @@ class RAGVectorStore:
         
         print("Documents Split")
         
-        if not file_path:
+        if not json_path:
             file_path = "chunked_data" + time.strftime("%Y%m%d-%H%M%S")
         
         with open(file_path, 'w') as jsonl_file:
