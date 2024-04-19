ef12fe146f6c24d4dada79d99aa67d2feec6947c
NMBridges <nolanmbridges@gmail.com>
2024-04-19 16:21:06 -0400

    dolly ralm update

--- src/dolly_ralm.py
+++ src/dolly_ralm.py
@@ -10,18 +10,15 @@ class DollyRALM(RALM):
     '''RALM class dedicated to wrapping a Transformer architecture (Dolly) for RAG tasks'''
     def __init__(self, pretrained_prefix : str = "databricks/dolly-v2-3b", vector_db : VectorStore = None):
         super().__init__(vector_db)
-        # TODO: Instantiate Dolly RALM model
 
         self.lm = pipeline(model=pretrained_prefix, torch_dtype=torch.bfloat16, trust_remote_code=True, device_map=device)
 
     def format_output(self, output : str) -> str:
-
         return output[0]["generated_text"]
 
     #override
     def generate_prompt(self, question : str, k : int = 4) -> str:
         '''Generates an LLM prompt (context, question, specifications), provided the question, utilizing k context chunks'''
-        
         context = " , ".join(self.retrieve_context(question, k))
 
         messages = PromptTemplate(
@@ -44,16 +41,12 @@ class DollyRALM(RALM):
         return messages.format(context=context, question=question)
 
     def predict(self, question : str, k : int = 4) -> str:
-
         prompt = self.generate_prompt(question, k=k)
         response = self.lm(prompt)
 
         try:
-
             return self.format_output(response)
-
         except:
-            
             print("Error formatting output. Raw output string returned.")
             return response
             
